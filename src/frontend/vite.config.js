0068a1ba192f31c5d4213867788c71e5508395a3
Moneyfloyd214 <cresttrailllc@gmail.com>
2026-05-28 17:53:18 -0500

    Updated vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
   build: {
     emptyOutDir: true,
     sourcemap: false,
     minify: false,
+  },
+  define: {
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+      env.VITE_SUPABASE_URL || "https://eszmpjgmwjaxacmtugbz.supabase.co"
+    ),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+      env.VITE_SUPABASE_ANON_KEY ||
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzem1wamdtd2pheGFjbXR1Z2J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTM0NTgsImV4cCI6MjA5NTQ2OTQ1OH0.2A4LcfW1t6wIUi3XNf-QKQHWlOPsn2TR0Ms0ByyTELo"
+    ),
   },
   css: {
     postcss: "./postcss.config.js",
@@ -30,4 +41,5 @@ export default defineConfig({
       },
     ],
   },
+  };
 });
