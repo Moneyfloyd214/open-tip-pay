4321bf86dab47beee1ee521eda7776b4bbc00c2a
Moneyfloyd214 <cresttrailllc@gmail.com>
2026-05-28 22:05:17 -0500

    Updated vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -11,16 +11,9 @@ export default defineConfig(({ mode }) => {
     minify: false,
   },
   define: {
-    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
-      env.VITE_SUPABASE_URL || "https://eszmpjgmwjaxacmtugbz.supabase.co"
-    ),
-    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
-      env.VITE_SUPABASE_ANON_KEY ||
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzem1wamdtd2pheGFjbXR1Z2J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTM0NTgsImV4cCI6MjA5NTQ2OTQ1OH0.2A4LcfW1t6wIUi3XNf-QKQHWlOPsn2TR0Ms0ByyTELo"
-    ),
-    "import.meta.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(
-      env.VITE_CLERK_PUBLISHABLE_KEY || "pk_test_bW9kZXN0LWFudGVhdGVyLTQ3LmNsZXJrLmFjY291bnRzLmRldiQ"
-    ),
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    "import.meta.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY),
   },
   css: {
     postcss: "./postcss.config.js",
